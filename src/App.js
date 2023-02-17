360a36de97380624d7da54136cf1e36a5d5d4492
WParsec <91538926+WParsec@users.noreply.github.com>
2023-02-17 23:11:14 +0100

    ouf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
 import Home from "./routes/Home";
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
