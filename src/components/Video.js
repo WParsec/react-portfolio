602b90e141a862e51bc68fd1ec859eef36002c11
WParsec <91538926+WParsec@users.noreply.github.com>
2023-02-18 14:33:21 +0100

    tries again

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,78 +4,71 @@ import { Link } from "react-router-dom";
 
 import bgVideo from "../assets/videos/bg-art.mp4";
 
-const Video = () => {
-    return (
-        <div className="hero">
-            <video playsinline autoplay muted loop id="video" poster='../assets/fallback.jpg'>
-                <source src={bgVideo} type="video/mp4" />
-            </video>
-            <div className="hero-content">
-                <div className="hero-text">
-                    <p className="signature">Tom Ertvaag</p>
-                    <h1>Design & Development</h1>
-                </div>
-                <div className="hero-btns">
-                    <Link to="/projects" className="btn">Projects</Link>
-                    <Link to="/contact" className="btn">Contact</Link>
-                    <Link to="/about" className="btn">About</Link>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default Video;
-
-
-// function Video() {
-//     const [lowPowerMode, setLowPowerMode] = useState(false);
-  
-//     useEffect(() => {
-//       const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-  
-//       setLowPowerMode(mediaQuery.matches);
-  
-//       const handleChange = (event) => {
-//         setLowPowerMode(event.matches);
-//       };
-  
-//       mediaQuery.addEventListener('change', handleChange);
-  
-//       return () => {
-//         mediaQuery.removeEventListener('change', handleChange);
-//       };
-//     }, []);
-  
+// const Video = () => {
 //     return (
-//       <div className="hero">
-//         {lowPowerMode ? (
-//           <img src="../assets/fallback.jpg" alt="Fallback Image" />
-//         ) : (
-//           <video autoPlay loop muted playsInline id="video" poster="../assets/fallback.jpg">
-//             <source src={bgVideo} type="video/mp4" />
-//           </video>
-//         )}
-//         <div className="hero-content">
-//           <div className="hero-text">
-//             <p className="signature">Tom Ertvaag</p>
-//             <h1>Design & Development</h1>
-//           </div>
-//           <div className="hero-btns">
-//             <Link to="/projects" className="btn">
-//               Projects
-//             </Link>
-//             <Link to="/contact" className="btn">
-//               Contact
-//             </Link>
-//             <Link to="/about" className="btn">
-//               About
-//             </Link>
-//           </div>
+//         <div className="hero">
+//             <video autoplay playsinline muted loop id="video" poster='../assets/fallback.jpg'>
+//                 <source src={bgVideo} type="video/mp4" />
+//             </video>
+//             <div className="hero-content">
+//                 <div className="hero-text">
+//                     <p className="signature">Tom Ertvaag</p>
+//                     <h1>Design & Development</h1>
+//                 </div>
+//                 <div className="hero-btns">
+//                     <Link to="/projects" className="btn">Projects</Link>
+//                     <Link to="/contact" className="btn">Contact</Link>
+//                     <Link to="/about" className="btn">About</Link>
+//                 </div>
+//             </div>
 //         </div>
-//       </div>
 //     );
-//   }
+// }
+
+// export default Video;
+
+
+const Video = () => {
+    const [lowPowerMode, setLowPowerMode] = useState(false);
   
-//   export default Video;
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
   
+      setLowPowerMode(mediaQuery.matches);
+  
+      const handleChange = (event) => {
+        setLowPowerMode(event.matches);
+      };
+  
+      mediaQuery.addEventListener('change', handleChange);
+  
+      return () => {
+        mediaQuery.removeEventListener('change', handleChange);
+      };
+    }, []);
+  
+    return (
+      <div className="hero">
+        {lowPowerMode ? (
+          <img src="../assets/fallback.jpg" alt="Fallback Image" />
+        ) : (
+          <video autoPlay loop muted playsInline id="video" poster='../assets/fallback.jpg'>
+            <source src={bgVideo} type="video/mp4" />
+          </video>
+        )}
+        <div className="hero-content">
+          <div className="hero-text">
+            <p className="signature">Tom Ertvaag</p>
+            <h1>Design & Development</h1>
+          </div>
+          <div className="hero-btns">
+            <Link to="/projects" className="btn">Projects</Link>
+            <Link to="/contact" className="btn">Contact</Link>
+            <Link to="/about" className="btn">About</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Video;
