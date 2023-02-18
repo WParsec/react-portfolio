d55f0fb7a169b4837c90b4ee905a60f1aff75654
WParsec <91538926+WParsec@users.noreply.github.com>
2023-02-18 13:51:54 +0100

    edit

--- src/components/Video.js
+++ src/components/Video.js
@@ -29,46 +29,53 @@ import bgVideo from "../assets/videos/bg-art.mp4";
 
 
 function Video() {
-  const [lowPowerMode, setLowPowerMode] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-
-    setLowPowerMode(mediaQuery.matches);
-
-    const handleChange = (event) => {
-      setLowPowerMode(event.matches);
-    };
-
-    mediaQuery.addEventListener('change', handleChange);
-
-    return () => {
-      mediaQuery.removeEventListener('change', handleChange);
-    };
-  }, []);
-
-  return (
-    <div className="hero">
+    const [lowPowerMode, setLowPowerMode] = useState(false);
+  
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+  
+      setLowPowerMode(mediaQuery.matches);
+  
+      const handleChange = (event) => {
+        setLowPowerMode(event.matches);
+      };
+  
+      mediaQuery.addEventListener('change', handleChange);
+  
+      return () => {
+        mediaQuery.removeEventListener('change', handleChange);
+      };
+    }, []);
+  
+    return (
+      <div className="hero">
         {lowPowerMode ? (
-        <img src="../assets/bg-static.jpg" alt="Fallback Image" />
-      ) : (
-        <video autoPlay loop muted id="video">
+          <img src="../assets/bg-static.jpg" alt="Fallback Image" />
+        ) : (
+          <video autoPlay loop muted id="video">
             <source src={bgVideo} type="video/mp4" />
-        </video>
-      )}
+          </video>
+        )}
         <div className="hero-content">
-            <div className="hero-text">
-                <p className="signature">Tom Ertvaag</p>
-                <h1>Design & Development</h1>
-            </div>
-            <div className="hero-btns">
-                <Link to="/projects" className="btn">Projects</Link>
-                <Link to="/contact" className="btn">Contact</Link>
-                <Link to="/about" className="btn">About</Link>
-            </div>
+          <div className="hero-text">
+            <p className="signature">Tom Ertvaag</p>
+            <h1>Design & Development</h1>
+          </div>
+          <div className="hero-btns">
+            <Link to="/projects" className="btn">
+              Projects
+            </Link>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+            <Link to="/about" className="btn">
+              About
+            </Link>
+          </div>
         </div>
-    </div>
-  );
-}
-
-export default Video;
+      </div>
+    );
+  }
+  
+  export default Video;
+  
