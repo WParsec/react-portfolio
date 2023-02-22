42b231976ca929b8c9256bc04e7f5950dc2f90e0
WParsec <91538926+WParsec@users.noreply.github.com>
2023-02-22 16:17:10 +0100

    adds function to display video on mobile

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,16 +3,23 @@ import "./VideoStyles.css";
 import { Link } from "react-router-dom";
 
 import bgVideo from "../assets/videos/bg-art.mp4";
-import fallbackImage from '../assets/fallback.jpg';
 
 
 const Video = () => {
+
+    function showVideo() {
+        const video = document.getElementById("video");
+        video.style.display = "block";
+        video.play();
+    }
+
     return (
         <div className="hero">
             <video autoPlay muted loop playsInline id="video" poster='../assets/fallback.jpg'>
                 <source src={bgVideo} type="video/mp4" />
             </video>
             <div className="hero-content">
+                <p onClick={showVideo} className='play'>Play</p>
                 <div className="hero-text">
                     <p className="signature">Tom Ertvaag</p>
                     <h1>Design & Development</h1>
